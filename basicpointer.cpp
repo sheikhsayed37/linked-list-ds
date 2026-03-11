eaaf291d41982af80b5d1df0fc9d83d8f8935cb4
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-03-11 12:40:12 +0600

    8

---
+++ basicpointer.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void fun(int *ptr )
+{
+    int y=200;
+    ptr=&y;
+    cout<<"in fun"<<" "<<*ptr<<endl;
+}
+int main()
+{
+    int x=10;
+    int *ptr=&x;  //pointer er maddhome x er address ke ptr e rakhlam
+    fun(ptr);
+    cout<<x<<endl;
+    cout<<ptr<<endl;  //ptr er value print korbe mane x er address print korbe
+    cout<<*ptr<<endl; //ptr er value print korbe mane x er value
+}        
