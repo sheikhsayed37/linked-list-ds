4422d9fb55cf3763c9c121f0df539238e9f248b6
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2026-03-11 14:53:26 +0600

    12

---
+++ basicpointer3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void fun(int *&p) //p er reference pass korlam mane p er address pass korlam
+{
+    p=NULL;
+
+}
+int main()
+{
+    int x=10;
+    int *p=&x;
+    fun(p);
+    cout<<"in main "<<p<<endl;
+}
